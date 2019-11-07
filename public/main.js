d8ce2e09975b9e44b67e0e454f444d22ab6cada3
Nathanael Swander <swander926@yahoo.com>
2019-11-06 22:43:00 -0500

    shuffle deck and select random card on button click

--- public/main.js
+++ public/main.js
@@ -1,7 +1,39 @@
+const deck = []
+const suits = ['Spades', 'Diamonds', 'Clubs', 'Hearts']
+const values = [
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K'
+]
+let temp
+let randomNum
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      let temp = values[j] + ' of ' + suits[i]
+      deck.push(temp)
+    }
+  }
+  for (let z = 0; z < deck.length; z++) {
+    randomNum = Math.floor(Math.random() * z)
+
+    const holder = deck[randomNum]
+    deck[randomNum] = deck[z]
+    deck[z] = holder
   }
 }
-
+const dealCard = () => {
+  document.querySelector('.card-display').textContent = deck.pop()
+}
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.select-card').addEventListener('click', dealCard)
